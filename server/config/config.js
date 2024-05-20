278b53573f0a4e271894eac7d12aae646f59cb4f
0Prince-Gupta0 <prince17072003@gmail.com>
2024-05-20 18:45:28 +0530

    Third Commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './c.env' });
+dotenv.config({ path:`${__dirname}/../c.env`});
 
 const connectDB = async ()=>{
     const DB = process.env.MONGO_URI.replace('<PASSWORD>', process.env.PASSWORD);
--- server/server.js
+++ server/server.js
@@ -1,42 +1,39 @@
-const express=require("express");
-const dotenv= require("dotenv");
-const connectDB=require("./config/config");
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/config");
 require("colors");
-const morgan=require("morgan");
-const mongoose = require('mongoose');
- // updates
-var cors = require('cors');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+var cors = require('cors')
+const app = express();
 
-app.use(cors())
- // updates
-
-
-dotenv.config({ path: './config.env' });
-const DB = process.env.MONGO_URI.replace('<PASSWORD>', process.env.PASSWORD);
+dotenv.config({ path: "./c.env" });
+const DB = process.env.MONGO_URI.replace("<PASSWORD>", process.env.PASSWORD);
 
 mongoose.connect(DB).then(() => {
-  console.log('DB connection successful!!!');
+  console.log("DB connection successful!!!");
 });
 
-const app=express();
 
+
+
+app.use(cors()) // Use this after the variable declaration
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use("/api/pizzas", require("./routes/pizzaRoute"));
 app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/orders", require("./routes/orderRoute"));
-app.get("/",()=>{
-  // updates
+app.get("/", () => {
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-   // updates
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+ 
 });
 
-const port =process.env.PORT || 8080
-app.listen(port, ()=>{
- console.log(`Server Running on ${process.env.PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server Running on ${process.env.PORT}`);
 });
